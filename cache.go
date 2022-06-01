c92a035ee70b3a11d2dff6cdf7f5726782ff4115
Manukyan, Kristina <Kristina_Manukyan@epam.com>
2022-06-01 22:10:58 +0400

    Initial commit

--- cache.go
+++ cache.go
@@ -2,22 +2,50 @@ package cache
 
 import "time"
 
+type myType struct {
+	t time.Time
+	v string
+}
 type Cache struct {
+	m map[string] myType
+	b bool
+	
 }
 
 func NewCache() Cache {
 	return Cache{}
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func (c Cache) Get(key string) (string, bool) {
+	return c.m[key].v, c.b
 }
 
-func (receiver) Put(key, value string) {
+func (c *Cache) Put(key, value string) {
+	if thisKey, ok := c.m[key]; ok {   // https://webapplicationconsultant.com/go-lang/cannot-assign-to-struct-field-in-map/
+		thisKey.v = value
+		c.m[key] = thisKey 
+	}
+	
 }
 
-func (receiver) Keys() []string {
+func (c Cache) Keys() []string {
+	var s[]string
+	for k, _ := range c.m {
+		s = append(s, k)
+	}
+	return s
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c *Cache) PutTill(key, value string, deadline time.Time) {
+	if thisKey, ok := c.m[key]; ok {   // https://webapplicationconsultant.com/go-lang/cannot-assign-to-struct-field-in-map/
+		thisKey.v = value
+		c.m[key] = thisKey 
+	}
+	if thisTime, ok := c.m[key]; ok {   // https://webapplicationconsultant.com/go-lang/cannot-assign-to-struct-field-in-map/
+		thisTime.t = deadline
+		c.m[key] = thisTime 
+	}
+
+
+	
 }
